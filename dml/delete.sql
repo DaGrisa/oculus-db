0dd5045afad65697d8097cafd531d9a45d89c9fa
DaGrisa <dgr7348@students.fhv.at>
2015-03-26 22:42:35 +0100

    testdata

---
+++ dml/delete.sql
@@ -0,0 +1,8 @@
+/* delete.sql
+DML to delete all testdata.
+@creator dgr
+@history	2015-03-26	dgr	create
+*/
+
+delete from patient;
+delete from usergroup;
--- dml/patient.sql
+++ dml/patient.sql
@@ -4,7 +4,5 @@ DML to create patient testdata.
 @history	2015-03-26	dgr	create
 */
 
-TRUNCATE TABLE patient;
-
-INSERT INTO patient (patientId, doctorId, svn, firstName, lastName, birthDay, gender, street, postalCode, city, phone, email, allergy, childhoodAilments, medicineIntolerance) 
-VALUES (NULL, NULL, '1234010170', 'Donald', 'Duck', '1970-03-01', 'M', 'Entenstraße 43g Stiege 7 Top 9', '378x93', 'Entenhausen', '004366477263549', 'donald.duck@quack.eh', 'Glück', 'Vogelgrippe', 'Polypropylenacetylmonochlorid');
+INSERT INTO patient (patientId, doctorId, svn, firstName, lastName, birthDay, gender, street, postalCode, city, phone, email, allergy, childhoodAilments, medicineIntolerance) VALUES 
+(NULL, NULL, '1234010170', 'Donald', 'Duck', '1970-03-01', 'M', 'Entenstraße 43g Stiege 7 Top 9', '378x93', 'Entenhausen', '004366477263549', 'donald.duck@quack.eh', 'Glück', 'Vogelgrippe', 'Polypropylenacetylmonochlorid');
