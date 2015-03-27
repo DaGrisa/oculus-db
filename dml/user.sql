18c1f06641c5bfec629c2c29d93e00bd009339ce
Arno <arno.riedmann@gmx.at>
2015-03-27 21:29:39 +0100

    utf8

--- dml/user.sql
+++ dml/user.sql
@@ -11,5 +11,5 @@ INSERT INTO user (userId, userGroupId, userName, password, realName, createDate,
   (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'xyz7890', 'password', 'Xavier Yzir', DEFAULT, NULL),
   (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort1234', 'password', 'Otto Rtula', DEFAULT, NULL),
   (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort5678', 'password', 'Omar Rtatata', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Sprechstundenhilfe'), 'spr1357', 'password', 'Sahra Pröll', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Sprechstundenhilfe'), 'spr1357', 'password', 'Sahra PrÃ¶ll', DEFAULT, NULL),
   (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Administrator'), 'adm1337', 'password', 'Admin', DEFAULT, NULL);
