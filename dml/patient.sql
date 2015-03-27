f076d0c94729c69369e91eb4f27b986c2ae462d1
Caroooo <caroline.meusburger@students.fhv.at>
2015-03-27 10:35:05 +0100

    test commit

--- dml/patient.sql
+++ dml/patient.sql
@@ -5,4 +5,4 @@ DML to create patient testdata.
 */
 
 INSERT INTO patient (patientId, doctorId, svn, firstName, lastName, birthDay, gender, street, postalCode, city, phone, email, allergy, childhoodAilments, medicineIntolerance) VALUES 
-(NULL, NULL, '1234010170', 'Donald', 'Duck', '1970-03-01', 'M', 'Entenstraße 43g Stiege 7 Top 9', '378x93', 'Entenhausen', '004366477263549', 'donald.duck@quack.eh', 'Glück', 'Vogelgrippe', 'Polypropylenacetylmonochlorid');
+(NULL, NULL, '1234010170', 'Donald', 'Duck', '1970-03-01', 'M', 'Entenstraße 43g Stiege 7 Top 9', '378x93', 'Entenhausen', '004366477263549', 'donald.duck@quack.eh', 'Glück', 'Vogelgrippe', 'Polypropylenacetylmonochlorid'); 
