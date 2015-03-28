bb5c3aa68d346d7a0b9a4a46d93356afad357062
Arno <arno.riedmann@gmx.at>
2015-03-28 11:05:35 +0100

    calendarEvent testdata wip

---
+++ dml/calendarEvent.sql
@@ -0,0 +1,9 @@
+/* calendarevent.sql
+DML to create calendarEvent testdata.
+@creator ari
+@history	2015-03-28  ari create
+*/
+
+INSERT INTO calendarEvent (calendarEventId, calendarId, patientId, eventTypeId, startEvent, endEvent, description) VALUES (
+
+);
