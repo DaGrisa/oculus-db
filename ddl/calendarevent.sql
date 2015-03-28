6493fbc5042c3159deff2cf272fc09938cd0f655
Arno <arno.riedmann@gmx.at>
2015-03-28 11:04:18 +0100

    required fields

--- ddl/calendarevent.sql
+++ ddl/calendarevent.sql
@@ -2,15 +2,16 @@
 DDL to create patient table.
 @creator dgr
 @history	2015-03-27	dgr	create
+          2015-03-28  ari change
 */
 
 CREATE TABLE calendarEvent (
 calendarEventId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-calendarId INT(6) UNSIGNED,
+calendarId INT(6) UNSIGNED NOT NULL,
 patientId INT(6) UNSIGNED,
-eventTypeId INT(6) UNSIGNED,
-startEvent DATETIME,
-endEvent DATETIME,
+eventTypeId INT(6) UNSIGNED NOT NULL,
+eventStart DATETIME NOT NULL,
+eventEnd DATETIME NOT NULL,
 description TEXT,
 FOREIGN KEY (calendarId) REFERENCES calendar(calendarId),
 FOREIGN KEY (patientId) REFERENCES patient(patientId),
