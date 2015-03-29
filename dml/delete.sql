c21708ab30444baa2765b66352f883bce0e2262d
Arno <arno.riedmann@gmx.at>
2015-03-29 10:20:41 +0200

    restored order

--- dml/delete.sql
+++ dml/delete.sql
@@ -4,28 +4,31 @@ DML to delete all testdata.
 @history	2015-03-26	dgr	create
 */
 
-DELETE FROM usergroup;
-DELETE FROM user;
-DELETE FROM calendarworkinghours;
-DELETE FROM calendar;
-DELETE FROM doctor;
-DELETE FROM patient;
-DELETE FROM orthoptist;
-DELETE FROM receptionist;
-DELETE FROM administrator;
-DELETE FROM eventtype;
-DELETE FROM calendarevent;
-DELETE FROM queue;
-DELETE FROM permission;
-DELETE FROM userpermission;
-DELETE FROM diagnosis;
 DELETE FROM examinationprotocol;
 DELETE FROM medicine;
+DELETE FROM diagnosis;
+DELETE FROM eventtype;
+UPDATE queue SET queueIdParent = NULL;
+DELETE FROM queue;
+DELETE FROM administrator;
+DELETE FROM receptionist;
+DELETE FROM orthoptist;
+DELETE FROM servicecode;
+DELETE FROM patient;
+DELETE FROM doctor;
+DELETE FROM calendar;
+DELETE FROM calendarworkinghours;
+DELETE FROM user;
+DELETE FROM usergroup;
+
+/* noch fehlende deletes
+DELETE FROM calendarevent;
+DELETE FROM permission;
+DELETE FROM userpermission;
 DELETE FROM prescription;
 DELETE FROM prescriptionentry;
-DELETE FROM servicecode;
 DELETE FROM examinationprotocolservicecode;
 DELETE FROM workdisability;
 DELETE FROM visualaid;
 DELETE FROM referralletter;
-DELETE FROM examinationresult;
+DELETE FROM examinationresult;*/
