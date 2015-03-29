07ee04957c492b6647ee2db7105cdf1c67e931c3
Arno <arno.riedmann@gmx.at>
2015-03-29 15:48:33 +0200

    eventTypeName NOT NULL

--- ddl/eventtype.sql
+++ ddl/eventtype.sql
@@ -2,11 +2,12 @@
 DDL to create eventtype table.
 @creator dgr
 @history	2015-03-27	dgr	create
+          2015-03-29  ari require name
 */
 
 CREATE TABLE eventType (
 eventTypeId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-eventTypeName VARCHAR(50) UNIQUE,
+eventTypeName VARCHAR(50) UNIQUE NOT NULL,
 estimatedTime INT(6) UNSIGNED,
 description TEXT
 );
