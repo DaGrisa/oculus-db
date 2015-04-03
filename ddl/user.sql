f6f46a9fe7fbc9389c9067ce638c783a712f21e1
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-04-03 13:08:34 +0200

    Change User
    Add title, firstName, lastName, email

--- ddl/user.sql
+++ ddl/user.sql
@@ -2,6 +2,7 @@
 DDL to create user table.
 @creator dgr
 @history	2015-03-26	dgr	create
+			2015-04-03  dsc add title, firstName, lastName, email
 */
 
 CREATE TABLE user (
@@ -9,7 +10,10 @@ userId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 userGroupId INT(6) UNSIGNED,
 userName VARCHAR(30) NOT NULL,
 password VARCHAR(30) NOT NULL,
-realName VARCHAR(100) NOT NULL,
+title VARCHAR(30),
+firstName VARCHAR(50) NOT NULL,
+lastName VARCHAR(50) NOT NULL,
+email VARCHAR(255),
 createDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 idleDate DATETIME,
 FOREIGN KEY (userGroupId) REFERENCES userGroup(userGroupId)
--- dml/user.sql
+++ dml/user.sql
@@ -4,12 +4,12 @@ DML to create user testdata.
 @history	2015-03-27	ari	create
 */
 
-INSERT INTO user (userId, userGroupId, userName, password, realName, createDate, idleDate) VALUES
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'pta1234', 'password', 'Paul Tavolato', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'dru8472', 'password', 'David Ruben', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'abc1234', 'password', 'Aaron Bcahir', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'xyz7890', 'password', 'Xavier Yzir', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort1234', 'password', 'Otto Rtula', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort5678', 'password', 'Omar Rtatata', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Sprechstundenhilfe'), 'spr1357', 'password', 'Sahra Pröll', DEFAULT, NULL),
-  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Administrator'), 'adm1337', 'password', 'Admin', DEFAULT, NULL);
+INSERT INTO user (userId, userGroupId, userName, password, title, firstName, lastName, email, createDate, idleDate) VALUES
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'pta1234', 'password', 'Dr', 'Paul', 'Tavolato', 'paul.tavolato@hotmail.com', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'dru8472', 'password', 'Dr', 'David', 'Ruben', 'david.ruben@aol.com', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'abc1234', 'password', 'Dr', 'Aaron', 'Bcahir', 'aaron.bcahir@gmail.com', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Arzt'), 'xyz7890', 'password', 'Dr', 'Xavier', 'Yzir', NULL, DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort1234', 'password', 'Mag', 'Otto', 'Rtula', 'otto.rtula@yahoo.com', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Orthoptist'), 'ort5678', 'password', NULL, 'Omar', 'Rtatata', NULL, DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Sprechstundenhilfe'), 'spr1357', 'password', NULL, 'Sahra', 'Pröll', 'sahra.proell@gmail.com', DEFAULT, NULL),
+  (NULL, (SELECT userGroupId FROM usergroup WHERE userGroupName = 'Administrator'), 'adm1337', 'password', NULL, 'Anna', 'Dmening', 'anna.dmening@outlook.com', DEFAULT, NULL);
