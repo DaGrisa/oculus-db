59113b6698f47ec5999f17c26a37671518b72c48
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2015-04-22 13:51:03 +0200

    Add calendarEventId to queue

--- dml/queue.sql
+++ dml/queue.sql
@@ -5,17 +5,17 @@ DML to create queue testdata.
 */
 
 /* separate inserts für jeweils aktuelle werte für LAST_INSERT_ID() */
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Wayne'), NULL, STR_TO_DATE('8:20', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Anne'), LAST_INSERT_ID(), STR_TO_DATE('8:30', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Clarence'), LAST_INSERT_ID(), STR_TO_DATE('8:40', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'xyz7890')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Russell'),NULL, STR_TO_DATE('8:35', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, NULL, NULL, (SELECT patientId FROM patient WHERE firstName = 'Terry'), NULL, STR_TO_DATE('8:25', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, NULL, NULL, (SELECT patientId FROM patient WHERE firstName = 'Robin'), LAST_INSERT_ID(), STR_TO_DATE('8:40', '%H:%i'));
-INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, queueIdParent, arrivalTime) VALUES
-  (NULL, NULL, (SELECT orthoptistId FROM orthoptist WHERE userId = ((SELECT userId FROM user WHERE userName = 'ort1234'))), (SELECT patientId FROM patient WHERE firstName = 'Kathryn'), NULL, STR_TO_DATE('8:40', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Wayne'), NULL, NULL, STR_TO_DATE('8:20', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Anne'), NULL, LAST_INSERT_ID(), STR_TO_DATE('8:30', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'pta1234')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Clarence'), NULL, LAST_INSERT_ID(), STR_TO_DATE('8:40', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, (SELECT doctorId FROM doctor WHERE userId = (SELECT userId FROM user WHERE username = 'xyz7890')), NULL, (SELECT patientId FROM patient WHERE firstName = 'Russell'), NULL, NULL, STR_TO_DATE('8:35', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, NULL, NULL, (SELECT patientId FROM patient WHERE firstName = 'Terry'), NULL, NULL, STR_TO_DATE('8:25', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, NULL, NULL, (SELECT patientId FROM patient WHERE firstName = 'Robin'), NULL, LAST_INSERT_ID(), STR_TO_DATE('8:40', '%H:%i'));
+INSERT INTO queue (queueId, doctorId, orthoptistId, patientId, calendarEventId, queueIdParent, arrivalTime) VALUES
+  (NULL, NULL, (SELECT orthoptistId FROM orthoptist WHERE userId = ((SELECT userId FROM user WHERE userName = 'ort1234'))), (SELECT patientId FROM patient WHERE firstName = 'Kathryn'), NULL, NULL, STR_TO_DATE('8:40', '%H:%i'));
