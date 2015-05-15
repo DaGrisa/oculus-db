6dbe3f8108cd6f67e0f2c934fc0f5889b3d5d41c
Arno <arno.riedmann@gmx.at>
2015-05-15 15:20:05 +0200

    added two (optional) columns to medicine

--- ddl/medicine.sql
+++ ddl/medicine.sql
@@ -9,6 +9,8 @@ medicineId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 diagnosisId INT(6) UNSIGNED NOT NULL,
 name VARCHAR(100) NOT NULL,
 dose VARCHAR(255),
+startMedicine DATE,
+endMedicine DATE,
 FOREIGN KEY (diagnosisId) REFERENCES diagnosis(diagnosisId)
 );
 
