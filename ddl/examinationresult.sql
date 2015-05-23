18fb48ec47044ab1d37066c0f771b1e6e20c432b
Arno <arno.riedmann@gmx.at>
2015-05-23 14:50:36 +0200

    optionales sehtest zeug in examinationresult

--- ddl/examinationresult.sql
+++ ddl/examinationresult.sql
@@ -12,6 +12,11 @@ result TEXT,
 createDate DATETIME,
 device VARCHAR(255),
 deviceData BLOB,
+visualTestType VARCHAR(255),
+cylinderValue DECIMAL(6, 2),
+sphereValue DECIMAL(6, 2),
+axisValue DECIMAL(3, 0),
+prismValue DECIMAL(6, 2),
 FOREIGN KEY (examinationProtocolId) REFERENCES examinationProtocol(examinationProtocolId),
 FOREIGN KEY (userId) REFERENCES user(userId)
 );
